07d8e4329e280aba7dfe0db12ffc7afcd7e9cfe5
Benson Miakoun <bmiakoun@gmail.com>
2020-06-27 05:59:23 +0800

    "tested gpu:1"

---
+++ test.py
@@ -0,0 +1,14 @@
+import torch
+torch.cuda.is_available()
+
+print("a")
+
+if torch.cuda.is_available():  
+  dev = "cuda:0" 
+else:  
+  dev = "cpu"  
+device = torch.device(dev)  
+a = torch.zeros(4,3)    
+a = a.to(device)
+
+print(a)
